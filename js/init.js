82dfce2c55e6bb4fb960341763d4e8c952e5ce87
realxizhe <xizhe.official@gmail.com>
2020-05-03 12:14:34 +0800

    null

--- js/init.js
+++ js/init.js
@@ -1,7 +1,19 @@
 (function($){
   $(function(){
+    $("#return-top").hide();
+    $('.sidenav').sidenav();   
+  });
+ 
+  $(window).scroll(function(){
+    if($(this).scrollTop()>300){
+         $("#return-top").show()
+    }else{
+         $("#return-top").hide()
+    }
+  $(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 300});
+    
+  });
+})
 
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
 })(jQuery); // end of jQuery name space
