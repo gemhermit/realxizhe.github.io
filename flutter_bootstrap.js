a99f5959532c668b186dc790ec8ce466b84d395a
gemhermit <xizhe.official@gmail.com>
2024-11-09 15:52:21 +0800

    new

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "950670939"
+    serviceWorkerVersion: "1358119515"
   }
 });
